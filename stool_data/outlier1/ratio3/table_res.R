5e8c3f8300ff0b25bd7e2469b0e5535b4a4c9226
Guanxun Li <victorxun@r1.stat.tamu.edu>
2023-06-17 15:42:30 -0500

    Add stool data results.

--- stool_data/outlier1/ratio3/table_res.R
+++ stool_data/outlier1/ratio3/table_res.R
@@ -77,7 +77,7 @@ for (iter_set in seq_len(nset)) {
     "signal", signa_den, "streng", signa_streng, ".rds"
   ))
   res_table_conf <- data.frame(
-    "method" = c("LinDA", "LinDA97", "LinDA90", "LinDA_winsor", "Huber", "Bi-square", "QR"),
+    "method" = c("LinDA", "LinDA97", "LinDA90", "LinDA_winsor", "Huber", "Bi-square"),
     "confounder" = rep("with", n_method),
     "Power" = numeric(n_method), "Power_sd" = numeric(n_method),
     "FDR" = numeric(n_method), "FDR_sd" = numeric(n_method)
