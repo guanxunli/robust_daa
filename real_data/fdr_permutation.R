7e227d4bae3d79c87d416475138a778587811968
Guanxun Li <victorxun@r1.stat.tamu.edu>
2023-06-28 07:49:37 -0500

    Add real data.

--- real_data/fdr_permutation.R
+++ real_data/fdr_permutation.R
@@ -1,9 +1,8 @@
-set.seed(1)
 library(parallel)
 #### load dataset
 dta <- readRDS("real_data/datasets/CDI_IBD_RA_SMOKE.rds")
-preprocess_fun <- function(otu_tab, meta, prev_cut = 0.1, lib.cut = 1000) {
-  keep_sam <- colSums(otu_tab) >= lib.cut
+preprocess_fun <- function(otu_tab, meta, prev_cut = 0.1, lib_cut = 1000) {
+  keep_sam <- colSums(otu_tab) >= lib_cut
   Y <- otu_tab[, keep_sam]
   Z <- as.data.frame(meta[keep_sam, ])
   names(Z) <- names(meta)
@@ -12,13 +11,12 @@ preprocess_fun <- function(otu_tab, meta, prev_cut = 0.1, lib.cut = 1000) {
   Y <- Y[keep_tax, ]
   return(list(Y = Y, Z = Z, keep_sam = keep_sam, keep_tax = keep_tax))
 }
-thres_vec <- c(0.01, seq(0.05, 0.25, 0.05))
-n_thres <- length(thres_vec)
 
 #### Run different methods
-formulas <- c('Disease', 'Disease+Antibiotic', 'Disease')
+formulas <- c("Disease", "Disease+Antibiotic", "Disease")
 n_permu <- 1e3
 for (iter_dta in seq_len(3)) {
+  set.seed(1)
   ## load data set
   otu_tab <- dta[[2 * (iter_dta - 1) + 1]]
   meta <- dta[[2 * iter_dta]]
@@ -27,14 +25,16 @@ for (iter_dta in seq_len(3)) {
   meta <- dta_process$Z
   formula <- formulas[iter_dta]
   allvars <- colnames(meta)
-  out_res_list <- mclapply(seq_len(n_permu), function (iter_permu) {
-    meta_permu <- meta
-    meta_permu[, allvars] <- sample(meta[, allvars])
+  n_sample <- nrow(meta)
+  out_res_list <- mclapply(seq_len(n_permu), function(iter_permu) {
+    meta_permu <- as.data.frame(meta[sample(seq_len(n_sample)), ])
+    colnames(meta_permu) <- allvars
     out_res <- list()
     ## LinDA method
     linda_res <- LinDA::linda(otu_tab, meta_permu, paste("~", formula))
-    out_res[["linda"]] <- which(linda_res$output[[1]]$reject == TRUE)
-    
+    padj_linda <- linda_res$output[[1]]$padj
+    out_res[["linda"]] <- padj_linda
+
     ## LinDA97
     ## remove samples for Y
     otu_tab_tmp <- robustDAA::winsor.fun(otu_tab, 0.97)
@@ -49,8 +49,9 @@ for (iter_dta in seq_len(3)) {
       otu.tab = otu_tab_use, meta = meta_use, formula = paste("~", formula),
       winsor.quan = 0.97
     )
-    out_res[["linda97"]] <- which(linda97_res$output[[1]]$reject == TRUE)
-    
+    padj_linda97 <- linda97_res$output[[1]]$padj
+    out_res[["linda97"]] <- padj_linda97
+
     ## LinDA90
     ## remove samples for Y
     otu_tab_tmp <- robustDAA::winsor.fun(otu_tab, 0.90)
@@ -65,28 +66,62 @@ for (iter_dta in seq_len(3)) {
       otu.tab = otu_tab_use, meta = meta_use, formula = paste("~", formula),
       winsor.quan = 0.90
     )
-    out_res[["linda90"]] <- which(linda90_res$output[[1]]$reject == TRUE)
-    
+    padj_linda90 <- linda90_res$output[[1]]$padj
+    out_res[["linda90"]] <- padj_linda90
+
     ## Huber method
     huber_res <- robustDAA::rlm_fun(
       otu_tab = otu_tab, meta = meta_permu, formula = paste("~", formula),
       reg_method = "psi.huber"
     )
-    out_res[["huber"]] <- huber_res$index_select
-    
+    pvalue_huber <- huber_res$combine_pvalue
+    padj_huber <- p.adjust(pvalue_huber, method = "BH")
+    out_res[["huber"]] <- padj_huber
+
     ## Bisquare method
     bisquareres <- robustDAA::rlm_fun(
       otu_tab = otu_tab, meta = meta_permu, formula = paste("~", formula),
       reg_method = "psi.bisquare"
     )
-    out_res[["bisquare"]] <- bisquareres$index_select
-    
+    pvalue_bisquare <- bisquareres$combine_pvalue
+    padj_bisquare <- p.adjust(pvalue_bisquare, method = "BH")
+    out_res[["bisquare"]] <- padj_bisquare
+
     ## QR method
     qrres <- robustDAA::qr_fun(otu_tab = otu_tab, meta = meta_permu, paste("~", formula))
-    out_res[["qr"]] <- qrres$index_select
-    
+    pvalue_qr <- qrres$combine_pvalue
+    padj_qr <- p.adjust(pvalue_qr, method = "BH")
+    out_res[["qr"]] <- padj_qr
+
     ## return results
     return(out_res)
   }, mc.cores = 50)
-  saveRDS(out_res_list, paste0("real_data/results/data", iter_dta, "res.rds"))
+  saveRDS(out_res_list, paste0("real_data/results/data", iter_dta, "permu.rds"))
+}
+
+#### chekc FDR
+method_vec <- c("linda", "linda97", "linda90", "huber", "bisquare", "qr")
+n_method <- length(method_vec)
+alpha <- 0.1
+for (iter_dta in seq_len(3)) {
+  cat("Data set", iter_dta, "\n")
+  out_res_list <- readRDS(paste0("real_data/results/data", iter_dta, "permu.rds"))
+  n_permu <- length(out_res_list)
+  out_list <- list()
+  for (iter_method in method_vec) {
+    out_list[[iter_method]] <- 0
+  }
+  for (iter_permu in seq_len(n_permu)) {
+    out_res <- out_res_list[[iter_permu]]
+    for (iter_method in method_vec) {
+      padj <- out_res[[iter_method]]
+      n_select <- length(which(padj < alpha))
+      if (n_select > 0) {
+        out_list[[iter_method]] <- out_list[[iter_method]] + 1
+      }
+    }
+  }
+  fdr_vec <- as.numeric(unlist(out_list)) / n_permu
+  names(fdr_vec) <- method_vec
+  print(fdr_vec)
 }
--- real_data/plot_power.R
+++ real_data/plot_power.R
@@ -13,11 +13,9 @@ preprocess_fun <- function(otu_tab, meta, prev_cut = 0.1, lib.cut = 1000) {
   Y <- Y[keep_tax, ]
   return(list(Y = Y, Z = Z, keep_sam = keep_sam, keep_tax = keep_tax))
 }
-thres_vec <- c(0.01, seq(0.05, 0.25, 0.05))
-n_thres <- length(thres_vec)
 
 #### Run different methods
-formulas <- c('Disease', 'Disease+Antibiotic', 'Disease')
+formulas <- c("Disease", "Disease+Antibiotic", "Disease")
 for (iter_dta in seq_len(3)) {
   ## load data set
   otu_tab <- dta[[2 * (iter_dta - 1) + 1]]
@@ -26,12 +24,13 @@ for (iter_dta in seq_len(3)) {
   otu_tab <- dta_process$Y
   meta <- dta_process$Z
   formula <- formulas[iter_dta]
-  print(dim(otu_tab))
-  
+  out_res <- list()
+
   ## LinDA method
   linda_res <- LinDA::linda(otu_tab, meta, paste("~", formula))
   padj_linda <- linda_res$output[[1]]$padj
-  
+  out_res[["linda"]] <- padj_linda
+
   ## LinDA97
   ## remove samples for Y
   otu_tab_tmp <- robustDAA::winsor.fun(otu_tab, 0.97)
@@ -48,7 +47,8 @@ for (iter_dta in seq_len(3)) {
     winsor.quan = 0.97
   )
   padj_linda97 <- linda97_res$output[[1]]$padj
-  
+  out_res[["linda97"]] <- padj_linda97
+
   ## LinDA90
   ## remove samples for Y
   otu_tab_tmp <- robustDAA::winsor.fun(otu_tab, 0.90)
@@ -65,7 +65,8 @@ for (iter_dta in seq_len(3)) {
     winsor.quan = 0.90
   )
   padj_linda90 <- linda90_res$output[[1]]$padj
-  
+  out_res[["linda90"]] <- padj_linda90
+
   ## Huber method
   huber_res <- robustDAA::rlm_fun(
     otu_tab = otu_tab, meta = meta, formula = paste("~", formula),
@@ -73,7 +74,8 @@ for (iter_dta in seq_len(3)) {
   )
   pvalue_huber <- huber_res$combine_pvalue
   padj_huber <- p.adjust(pvalue_huber, method = "BH")
-  
+  out_res[["huber"]] <- padj_huber
+
   ## Bisquare method
   bisquareres <- robustDAA::rlm_fun(
     otu_tab = otu_tab, meta = meta, formula = paste("~", formula),
@@ -81,26 +83,47 @@ for (iter_dta in seq_len(3)) {
   )
   pvalue_bisquare <- bisquareres$combine_pvalue
   padj_bisquare <- p.adjust(pvalue_bisquare, method = "BH")
-  
+  out_res[["bisquare"]] <- padj_bisquare
+
   ## QR method
   qrres <- robustDAA::qr_fun(otu_tab = otu_tab, meta = meta, paste("~", formula))
   pvalue_qr <- qrres$combine_pvalue
   padj_qr <- p.adjust(pvalue_qr, method = "BH")
-  
-  ## save power
+  out_res[["qr"]] <- padj_qr
+
+  ## save results
+  saveRDS(out_res, paste0("real_data/results/data", iter_dta, "res.rds"))
+}
+
+#### power plot
+thres_vec <- c(0.01, seq(0.05, 0.25, 0.05))
+n_thres <- length(thres_vec)
+for (iter_dta in seq_len(3)) {
   power_mat <- matrix(NA, nrow = 6, ncol = n_thres)
+  out_res <- readRDS(paste0("real_data/results/data", iter_dta, "res.rds"))
+  padj_linda <- out_res[["linda"]]
+  padj_linda97 <- out_res[["linda97"]]
+  padj_linda90 <- out_res[["linda90"]]
+  padj_huber <- out_res[["huber"]]
+  padj_bisquare <- out_res[["bisquare"]]
+  padj_qr <- out_res[["qr"]]
+
   for (iter_thres in seq_len(n_thres)) {
     thres_use <- thres_vec[iter_thres]
-    power_mat[, iter_thres] <- c(sum(padj_linda <= thres_use),
-                                 sum(padj_linda97 <= thres_use),
-                                 sum(padj_linda90 <= thres_use),
-                                 sum(padj_huber <= thres_use),
-                                 sum(padj_bisquare <= thres_use),
-                                 sum(padj_qr <= thres_use))
+    power_mat[, iter_thres] <- c(
+      sum(padj_linda <= thres_use),
+      sum(padj_linda97 <= thres_use),
+      sum(padj_linda90 <= thres_use),
+      sum(padj_huber <= thres_use),
+      sum(padj_bisquare <= thres_use),
+      sum(padj_qr <= thres_use)
+    )
   }
-  df_plot <- data.frame(y = as.numeric(power_mat),
-                        method = rep(c("LinDA", "LinDA97", "LinDA90", "Huber", "Bi_square", "QR"), n_thres),
-                        x = rep(thres_vec, each = 6))
+  df_plot <- data.frame(
+    y = as.numeric(power_mat),
+    method = rep(c("LinDA", "LinDA97", "LinDA90", "Huber", "Bi_square", "QR"), n_thres),
+    x = rep(thres_vec, each = 6)
+  )
   df_plot$method <- factor(df_plot$method, levels = c(
     "LinDA", "LinDA97", "LinDA90",
     "Huber", "Bi_square", "QR"
@@ -112,8 +135,34 @@ for (iter_dta in seq_len(3)) {
     ylab("Number of discoveries") +
     theme_bw(base_size = 33) +
     theme(legend.position = "bottom")
-  ggsave(paste0("real_data/figures/realdata", iter_dta, ".pdf"),
-         p1,
-         width = 10, height = 10
+  ggsave(paste0("real_data/figures/power/realdata", iter_dta, ".pdf"),
+    p1,
+    width = 10, height = 10
   )
+}
+
+
+#### overlap plot
+library(UpSetR)
+alpha <- 0.1
+for (iter_dta in seq_len(3)) {
+  out_res <- readRDS(paste0("real_data/results/data", iter_dta, "res.rds"))
+  padj_linda <- out_res[["linda"]]
+  padj_linda97 <- out_res[["linda97"]]
+  padj_linda90 <- out_res[["linda90"]]
+  padj_huber <- out_res[["huber"]]
+  padj_bisquare <- out_res[["bisquare"]]
+  padj_qr <- out_res[["qr"]]
+  res_list <- list()
+  res_list[["LinDA"]] <- which(padj_linda < alpha)
+  res_list[["LinDA97"]] <- which(padj_linda97 < alpha)
+  res_list[["LinDA90"]] <- which(padj_linda90 < alpha)
+  res_list[["Huber"]] <- which(padj_huber < alpha)
+  res_list[["Bi_square"]] <- which(padj_bisquare < alpha)
+  res_list[["QR"]] <- which(padj_qr < alpha)
+  pdf(paste0("real_data/figures/overlap/realdata", iter_dta, ".pdf"),
+    width = 10, height = 10
+  )
+  print(upset(fromList(res_list), nintersects = 12))
+  dev.off()
 }
